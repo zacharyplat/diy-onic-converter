0ab38be21788fb1d40522c02fe2b5d3548edd82e
zacharyplat <zachary.plat@gmail.com>
2023-05-05 16:43:58 -0700

    Closer to a working example with a few bugs: content is duplicated (feature not a bug, need to hide old) bold tags are not handled correctly

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -9,7 +9,7 @@ const boldPrefixAsHtml = (text) => {
 const convertTags = (element) => {
   const textArray = element.innerText.split(" ");
   const htmlWrappedText = textArray.map((text) => boldPrefixAsHtml(text));
-  const newElement = document.createElement(element.tagName);
+  const newElement = element.cloneNode(true);
   newElement.innerHTML = htmlWrappedText.join(" ");
   return newElement;
 };
@@ -26,14 +26,15 @@ const convertAllTags = (element) => {
       );
     });
   }
-  debugger;
   return newElement;
 };
 const diyOnicConverter = (textContentContainerSelector) => {
   const container = document.querySelector(textContentContainerSelector);
   const elementsMapable = [...container.children];
-  elementsMapable.map((element) => convertAllTags(element));
-  elementsMapable.forEach((element) => container.appendChild(element));
+  const convertedElements = elementsMapable.map((element) =>
+    convertAllTags(element)
+  );
+  convertedElements.forEach((element) => container.appendChild(element));
 };
 
 // Allow global access so that this can be executed from the console.
