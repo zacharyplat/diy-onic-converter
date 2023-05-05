0dc7ca07f80ba3d6d7f888a228607d9b003025b4
zacharyplat <zachary.plat@gmail.com>
2023-05-05 13:59:55 -0700

    Created mvp of bionic reading with prefix 3, currently is destructive to any children html nodes inside the 'p' tags

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -1,12 +1,27 @@
 /**
  * Implement your converter function here.
  */
+const boldPrefixAsHtml = (text) => {
+  const prefix = text.slice(0, 3);
+  const rest = text.slice(3);
+  return `<strong>${prefix}</strong>${rest}`;
+};
+const convertPTags = (element) => {
+  const textArray = element.innerText.split(" ");
+  const htmlWrappedText = textArray.map((text) => boldPrefixAsHtml(text));
+  element.innerHTML = htmlWrappedText.join(" ");
+  return element;
+};
 const diyOnicConverter = (textContentContainerSelector) => {
   const container = document.querySelector(textContentContainerSelector);
   console.log("Performing bionic reading conversion on:", container);
-
-  /* Etc. etc. etc. your code etc. */
+  const elementsMapable = [...container.children];
+  elementsMapable.map((element) =>
+    element.tagName === "P" ? convertPTags(element) : element
+  );
+  elementsMapable.forEach((element) => container.appendChild(element));
 };
 
 // Allow global access so that this can be executed from the console.
 window.diyOnicConverter = diyOnicConverter;
+diyOnicConverter("body");
